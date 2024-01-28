f1f6ab8dd2bd0f0664c7d01d4ed1f99d61e2dc12
= <=mohandmk.mk@gmail.com>
2024-01-28 13:37:00 +0530

    changes in changing column name

--- mes/settings.py
+++ mes/settings.py
@@ -42,7 +42,6 @@ SYSTEM_APPS = [
 LOCAL_APPS=[
    'app',
     'accounts',
-    'utils',
             ]
 
 THIRD_PARTY_APPS=[
@@ -84,24 +83,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mes.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mes-dev-db',
-        'USER': 'postgres',
-        'PASSWORD': 'Gokul@4580',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-# import os
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mes-dev-db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Gokul@4580',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+import os
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
