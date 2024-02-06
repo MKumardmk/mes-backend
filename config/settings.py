f94690a47b93dd9d35b2d5191ab2186e1629a819
daisyponraj-rap <daisy@dartexon.com>
2024-02-06 16:18:04 +0530

    db changes

--- config/settings.py
+++ config/settings.py
@@ -90,22 +90,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mes',
-#         'USER': 'globev',
-#         'PASSWORD': 'globev',
-#         'HOST': '72.181.13.50',
-#         'PORT': '5435',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mes-dev-db',
+        'USER': 'postgres',
+        'PASSWORD': 'Test@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
